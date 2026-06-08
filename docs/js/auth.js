73ed905ac841b732883cbd35b801e4eb400dd326
Falcao92 <falkojaeger92@googlemail.com>
2026-06-08 15:15:30 +0200

    Update auth.js

--- docs/js/auth.js
+++ docs/js/auth.js
@@ -1,4 +1,4 @@
-const msalConfig={auth:{clientId:"f52a25f2-e683-4bea-ae0f-6206e05a9b60",authority:"https://login.microsoftonline.com/d05e1986-9d0f-4d67-8b0d-990eb3ae4ecd",redirectUri:window.location.origin}};
+const msalConfig={auth:{clientId:"f52a25f2-e683-4bea-ae0f-6206e05a9b60",authority:"https://login.microsoftonline.com/d05e1986-9d0f-4d67-8b0d-990eb3ae4ecd",redirectUri:window.location.origin + "/darts-app/"}};
 const msalInstance=new msal.PublicClientApplication(msalConfig);
 let accessToken=null;
 async function login(){const result=await msalInstance.loginPopup({scopes:["User.Read","Sites.ReadWrite.All"]});accessToken=result.accessToken;}
