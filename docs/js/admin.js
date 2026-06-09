76fcc67cc0c9cb5de2b2b5d7eec74fd6ff4252d2
Falcao92 <falkojaeger92@googlemail.com>
2026-06-09 12:34:06 +0200

    Update admin.js

--- docs/js/admin.js
+++ docs/js/admin.js
@@ -3,6 +3,21 @@ window.onload=async()=>{await login();loadPlayers();}
 
 async function loadPlayers(){const data=await getList('Players');document.getElementById('players').innerHTML=data.map(p=>p.fields.Title).join('<br>');}
 
+async function deletePlayer(id) {
+  const t = await getToken();
+
+  await fetch(
+    `https://graph.microsoft.com/v1.0/sites/${SITE_ID}/lists/Players/items/${id}`,
+  {
+    method:"DELETE",
+    headers:{ Authorization:`Bearer ${t}` }
+  });
+
+  loadPlayers();
+}
+
+
+
 async function createPlayer(){const t=await getToken();const n=document.getElementById('name').value;
 await fetch(`https://graph.microsoft.com/v1.0/sites/${SITE_ID}/lists/Players/items`,{method:'POST',headers:{Authorization:`Bearer ${t}`,'Content-Type':'application/json'},body:JSON.stringify({fields:{Title:n}})});
 loadPlayers();}
