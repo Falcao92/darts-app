e769fd711120ffe3cdba0f00cbf4c8e33818040f
Falcao92 <falkojaeger92@googlemail.com>
2026-06-09 09:12:58 +0200

    Create players-admin.js

---
+++ docs/js/players-admin.js
@@ -0,0 +1,56 @@
+async function loadPlayers() {
+  await login();
+
+  const res = await fetch(
+    `https://graph.microsoft.com/v1.0/sites/${SITE_ID}/lists/Players/items?expand=fields`,
+    {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    }
+  );
+
+  const data = await res.json();
+  const div = document.getElementById("players");
+
+  div.innerHTML = "";
+
+  data.value.forEach(p => {
+    div.innerHTML += `
+      <div style="margin-bottom:10px;">
+        <b>${p.fields.Title}</b>
+      </div>
+    `;
+  });
+}
+
+async function createPlayer() {
+  await login();
+
+  const name = document.getElementById("name").value;
+
+  if (!name) {
+    alert("Bitte Name eingeben");
+    return;
+  }
+
+  await fetch(
+    `https://graph.microsoft.com/v1.0/sites/${SITE_ID}/lists/Players/items`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        fields: {
+          Title: name
+        }
+      })
+    }
+  );
+
+  document.getElementById("name").value = "";
+
+  await loadPlayers();
+}
+
+loadPlayers();
