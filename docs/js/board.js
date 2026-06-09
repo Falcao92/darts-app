c0a9d6bef8de82f48b555dfc17751aacf94266ef
Falcao92 <falkojaeger92@googlemail.com>
2026-06-09 12:28:42 +0200

    Update board.js

--- docs/js/board.js
+++ docs/js/board.js
@@ -1,3 +1,41 @@
+const params = new URLSearchParams(window.location.search);
+const boardId = params.get("board");
 
-window.onload=async()=>{await login();load();}
-async function load(){const m=await getList('Matches');document.getElementById('data').innerText=JSON.stringify(m,null,2);} 
+window.addEventListener("DOMContentLoaded", async () => {
+  const ok = await ensureLogin();
+  if (!ok) return;
+
+  loadBoard();
+});
+
+async function loadBoard() {
+  const matches = await getList("Matches");
+
+  const match = matches.find(m => m.fields.BoardId == boardId);
+
+  if (!match) {
+    document.getElementById("content").innerHTML = "Kein Match";
+    return;
+  }
+
+  const f = match.fields;
+
+  document.getElementById("content").innerHTML = `
+    <h2>${f.Player1} vs ${f.Player2}</h2>
+    <h1>${f.Score1} : ${f.Score2}</h1>
+    <h2>${f.Turn === "p1" ? f.Player1 : f.Player2} ist dran</h2>
+    <h3>${getCheckout(f.Turn === "p1" ? f.Score1 : f.Score2)}</h3>
+  `;
+}
+
+
+function getCheckout(score) {
+  const map = {
+    170:"T20 T20 Bull",
+    100:"T20 D20",
+    80:"T20 D10",
+    40:"D20",
+    32:"D16"
+  };
+  return map[score] || "";
+}
