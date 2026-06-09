946b3803440d28d252c7a165c92e368740eb5f05
Falcao92 <falkojaeger92@googlemail.com>
2026-06-09 10:45:39 +0200

    Update auth.js

--- docs/js/auth.js
+++ docs/js/auth.js
@@ -1,11 +1,10 @@
 const msalConfig = {
   auth: {
-    clientId: "DEINE_CLIENT_ID",
-    authority: "https://login.microsoftonline.com/DEIN_TENANT_ID",
+    clientId: "f52a25f2-e683-4bea-ae0f-6206e05a9b60",
+    authority: "https://login.microsoftonline.com/d05e1986-9d0f-4d67-8b0d-990eb3ae4ecd",
     redirectUri: window.location.origin + "/darts-app/"
   }
 };
-
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
 let accessToken = null;
